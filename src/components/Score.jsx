aaf251764d29d2c1cb77d15c82bdf39ef14fab0d
Savelii Kosevich <savva0512@gmail.com>
2025-01-16 18:40:47 +0300

    created score component

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,18 @@
+import styles from "./score.module.css";
+
 export const Score = ({ nowScore, bestScore }) => {
   return (
-    <div className="score">
-      <p className="score__now">{nowScore}</p>
-      <p className="score__best">{bestScore}</p>
+    <div className={styles.score}>
+      <div className={styles.score__title}>
+        <h2 className={styles.title}>Pokemon Memory Game</h2>
+        <p className={styles.text}>
+          Get points by clicking on an image but don't click on any more than once!
+        </p>
+      </div>
+      <div className={styles.score__container}>
+        <p className={styles.score__now}>Score: {nowScore}</p>
+        <p className={styles.score__best}>Best score: {bestScore}</p>
+      </div>
     </div>
   );
 };
