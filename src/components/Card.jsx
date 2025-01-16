cd477f43c1f355e9716d74dbbeb7e724af0e7c6f
Savelii Kosevich <savva0512@gmail.com>
2025-01-16 17:33:31 +0300

    added classes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 export const Card = ({ data, clickFunc }) => {
   return (
-    <div>
+    <>
       {data ? (
-        <div onClick={() => clickFunc(data.id)}>
-          <h2>{data.name}</h2>
+        <div className="card" onClick={() => clickFunc(data.id)}>
+          <h2 className="card__title">{data.name}</h2>
           <p>{data.id}</p>
-          <img src={data.sprites.front_default} alt={data.name} />{" "}
+          <img className="card__img" src={data.sprites.front_default} alt={data.name} />{" "}
         </div>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </>
   );
 };
